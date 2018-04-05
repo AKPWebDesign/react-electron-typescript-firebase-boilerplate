d32cf599a1ac42d73d73250388b23ac85bc73d8e
Austin Peterson <austin@akpwebdesign.com>
2018-04-05 13:45:48 -0400

    feat(electron): devtools additions, use real URL

--- electron-wrapper/main.js
+++ electron-wrapper/main.js
@@ -10,16 +10,32 @@ const urls = {
   dev: 'http://localhost:3000',
 
   // replace with your firebase app URL (or whatever else you might be using)
-  live: 'https://my-fancy-app.firebaseapp.com',
+  live: 'https://react-electron-firebase.firebaseapp.com',
 };
 
+if (argv.dev) {
+  require('electron-debug')({ // eslint-disable-line global-require
+    showDevTools: 'undocked',
+    enabled: true,
+  });
+}
+
 function createWindow() {
+  if (argv.dev) {
+    // eslint-disable-next-line global-require
+    const { default: installExtension, REDUX_DEVTOOLS } = require('electron-devtools-installer');
+
+    installExtension(REDUX_DEVTOOLS)
+      .then(name => console.log(`Added Extension:  ${name}`))
+      .catch(err => console.log('An error occurred: ', err));
+  }
+
   // Create the browser window.
   const win = new BrowserWindow({ width: 1600, height: 900 });
 
   const winURL = argv.dev ? urls.dev : urls.live;
 
-  // and load the index.html of the app.
+  // and load the URL of the app.
   win.loadURL(winURL);
 }
 
